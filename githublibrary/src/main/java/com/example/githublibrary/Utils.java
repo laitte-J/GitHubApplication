d0843da00829b5e8d8b3bf25a6b4ad83d6f0c421
luodahai <xiaozhu20hua@yeah.net>
2017-06-20 14:02:36 +0800

    Initial commit

---
+++ githublibrary/src/main/java/com/example/githublibrary/Utils.java
@@ -0,0 +1,13 @@
+package com.example.githublibrary;
+
+/**
+ * @author Lenovo
+ * @desc
+ * @time 2017/6/20 11:12
+ */
+
+public class Utils {
+    public static String getVersion() {
+        return "v1.0";
+    }
+}
